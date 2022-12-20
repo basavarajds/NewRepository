467e8daa97591abf7726daa25853edc000ea4db1
basavaraj <bdiddege@gmail.com>
2022-12-20 20:22:28 +0530

    committed click and click1 function

--- NewRepository/src/Utils.java
+++ NewRepository/src/Utils.java
@@ -5,5 +5,14 @@ public class Utils {
 		System.out.println("Taking screenshot");
 		System.out.println("Branch_01");
 	}
-
+	public void click()
+	{
+		System.out.println("click");
+		System.out.println("Branch_01");
+	}
+	public void click1()
+	{
+		System.out.println("click1");
+		System.out.println("Branch_01");
+	}
 }
